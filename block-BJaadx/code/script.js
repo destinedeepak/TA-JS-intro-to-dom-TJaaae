4b4607610e9adec3a9db074d081a2d6d2e324ac2
Deepak Singh <destinedks@gmail.com>
2021-05-01 14:20:24 +0530

    update changes

--- block-BJaadx/code/script.js
+++ block-BJaadx/code/script.js
@@ -13,13 +13,14 @@ document.body.children[5].children[0].innerText = `all about document`
 document.body.children[6].children[2]
 
 // 3. Log the number (using console.log) of children of all the `li` element inside the ul with class `topics`
-console.log(document.querySelector("ul").childElementCount)
+let ul = document.body.children[5]
+console.log(ul.children.length)
 
 // 4. Select the first input using the `type` selector and store them in variable named `emailInput`
-let = emailInput = document.querySelector('input')
+let emailInput = document.querySelector('input')
 
 // 5. Select the ul element using class selector and store in `topics`
-let topics = document.querySelector('ul')
+let topics = document.querySelector('.topics')
 
 // 6. Select the first label element and store in `label`
 let label =document.querySelector(`label`)
@@ -42,47 +43,47 @@ let allInput = document.querySelectorAll("input")
 
 // 12. Use forEach to console the `innerText` property of all the li element in `allTopics` variable.
 allTopics.forEach(element =>{
-    console.log(element)
+    console.log(element.innerText)
     })
 
 // 13. Select all the elements with class `list` and store in variable `listOfSelectedTopics`
-let listOfSelectedTopics = document.getElementsByClassName("list")
+let listOfSelectedTopics = document.querySelectorAll("list")
 
 // 14. Select the first li element inside the `ul` element using `>` (direct child) and store in `firstLi`
 let firstLi = document.querySelector("ul>li")
 
 // 15. Select all the img element and log the number of element saying `The total number of img element is ---`
-console.log(`The total number of img element is ${document.querySelector("img").childElementCount}`)
+console.log(`The total number of img element is ${document.querySelectorAll("img").length}`)
 
 // 16. Select all the `p` element and store in `allPElement`
 let allPElement = document.querySelectorAll("p")
 
 // 17. Select all the buttons and log the count of buttons.
-// console.log(`count of buttons${document.querySelectorAll('btn')}`)
+console.log(`count of buttons: ${document.querySelectorAll('button').length}`)
 
 // 18. Select all the `label` element and log the count.
 console.log(`count of label: ${document.querySelectorAll('label').length}`)
 
 // 19. Select all the elements with `id` of `test`
-allPElementWithIdText = document.getElementById("test")
+allElementWithIdText = document.querySelectorAll("#test")
 
 // 20. Select the first element with id `test` using `getElementById`
-allPElementWithIdText = document.getElementById("test")
+fisrtElementWithIdText = document.getElementById("test")
 
 // 21. Select the parent element of the element stored in `topics` variable (#5) and log the element.
-console.log(topics.parentElement)
+console.log(topics.parentElement) 
 
 // 22. Select the next element sibling of the element stored in `topics` variable (#5) and log the element.
 console.log(topics.nextElementSibling)
 
 // 23. Select the previous element sibling of the element stored in `topics` variable (#5) and change the `innerText` property to `Learning About Walking the DOM`.
-topics.previousSibling.innerText = "Learning About Walking the DOM"
+topics.previousElementSibling.innerText = "Learning About Walking the DOM"
 
 // 24. Select the first element child of the element stored in `topics` variable (#5) and change the `innerText` property of the element to `This is the first child element`.
-topics.firstChild.innerText = "This is the first child element"
+topics.firstElementChild.innerText = "This is the first child element"
 
 // 25. Select the last element child of the element stored in `topics` variable (#5) and log the `typeof` the element.
-console.log(typeof topics.lastChild)
+console.log(typeof topics.lastElementChild)
 
 // 26. Select the element with type `fieldset` and store in a variable named `fieldsetElm`.
 let fieldsetElm = document.querySelector('fieldset')
