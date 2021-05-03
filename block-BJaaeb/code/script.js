ce429b2f8229aeb70999928c791456b5e88aa1f9
Deepak Singh <destinedks@gmail.com>
2021-05-03 16:03:12 +0530

    update changes

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -5,25 +5,12 @@ default value to be "text" and return the input element inside label. (create it
 
 */
 
-// function createInputElm (label, type = "text"){
-//   let label1 = document.createElement('label');
-//   label1.innerText = label;
-//   let input = document.createElement('input');
-//   input.type = type;  
-//   label1.append(input);
-//   document.querySelector('body').append(label1)
-//   // return label1;
-// }
-
-// // TEST
-// createInputElm('Your name'); //<label>Your name: <input type="text"></label>
-// createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
-
-// 2. Do the same thing as above using string literal like `<h1>Hello</h1>`
-
 function createInputElm (label, type = "text"){
   let label1 = document.createElement('label');
-  label1.innerHTML = `${label} <input type="text">`;
+  label1.innerText = label;
+  let input = document.createElement('input');
+  input.type = type;  
+  label1.append(input);
   document.querySelector('body').append(label1)
   return label1;
 }
@@ -32,18 +19,27 @@ function createInputElm (label, type = "text"){
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
 createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
 
+// 2. Do the same thing as above using string literal like `<h1>Hello</h1>`
+
+function createInputElm (label, type = "text"){
+  let html = `<label>${label} <input type=${type}></label>`;
+  document.querySelector('body').innerHTML = html
+  return html;
+}
+
+// TEST
+// createInputElm('Your name'); //<label>Your name: <input type="text"></label>
+createInputElm('Your age', 'text'); //<label>Your age: <input type="number"></label>
+
 // 3. Create a function named `createList` that accept and array of data like ['Mango', 'Apple', 'Banana'] and returns
 // the html for the link like <ul> <li>Mango</li>  <li>Apple</li>  <li>Banana</li> </ul>
 
 function createList(arr){
-  let ul = document.createElement('ul');
-  arr.forEach(element => {
-    li = document.createElement('li');
-    li.innerText = element;
-    ul.append(li);
-  });
-  document.querySelector('body').append(ul)
-  return ul
+  let html = `<ul>
+  ${arr.map(ele => `<li>${ele}</li>`).join("")
+}
+  </ul>`
+  return html;
 }
 
 // TEST
@@ -63,34 +59,28 @@ createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
 */
 
 function createTodoList(arr){
-  let ul = document.createElement('ul');
-  arr.forEach (element => {
-    let li = document.createElement('li');
+let html  = `<ul>
 
-    p = document.createElement('p');
-    p.innerText = element.name;
+${arr.map(ele => `
+ <li>
+  <p>${ele.name}</p>
+  <input type="checkbox" ${ele.isDone ? "checked" : "uncheked"} name="" id="">
+  <span>X</span>
+ </li>
+`)}
+</ul>`
 
-    let input = document.createElement('input');
-    input.type = "checkbox";
-    input.setAttribute(element.isDone ? "checked" : "unchecked","");
-    input.setAttribute('name'," ");
-    input.setAttribute('id'," ");
-  
-    let span = document.createElement('span');
-    span.innerText ="X";
-    li.append(p, input, span);
-    ul.append(li);
-  });
-return ul;
+document.querySelector('body').innerHTML = html
+return html
 }
 
 // TEST
-// createTodoList([
-//   { name: 'Learn DOM', isDone: false },
-//   { name: 'Learn JS', isDone: true },
-// ]);
-// createTodoList([
-//   { name: 'Learn DOM', isDone: false },
-//   { name: 'Learn React', isDone: true },
-//   { name: 'Learn JS', isDone: true },
-// ]);
+createTodoList([
+  { name: 'Learn DOM', isDone: false },
+  { name: 'Learn JS', isDone: true },
+]);
+createTodoList([
+  { name: 'Learn DOM', isDone: false },
+  { name: 'Learn React', isDone: true },
+  { name: 'Learn JS', isDone: true },
+]);
